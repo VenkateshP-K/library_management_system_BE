086531a28bf40392df7346b6a5f1c550d8830bd6
Venkatesh <venkateshpk06@gmail.com>
2024-11-18 16:04:43 +0530

    update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://library-management-system-be.onrender.com/api'],
+    origin: ['http://localhost:5173/api', 'https://library-management-system-be.onrender.com/api'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
