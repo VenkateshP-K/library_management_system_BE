069711581959ec4cb38c3b59875b004fd22ced44
Venkatesh <venkateshpk06@gmail.com>
2024-11-18 10:45:38 +0530

    update

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(cors());
+
+const corsOptions = {
+    origin: ['http://localhost:5173', 'https://library-management-system-be.onrender.com/api'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  };
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
 
 app.use('/api/users', userRouter);
 app.use('/api/books', bookRouter);
