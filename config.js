8579b31c0c95e560bcf00f1a80d464346881e05d
Venkatesh <venkateshpk06@gmail.com>
2024-11-21 19:01:10 +0530

    commit

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require ('dotenv').config();
 
     const MONGO_URI = process.env.MONGO_URI
     const PORT = process.env.PORT
-    const JWT_SECRET = process.env.JWT_secret
+    const JWT_SECRET = process.env.JWT_SECRET
  
 module.exports = {
     MONGO_URI,
